4f2b0686da4026b7e10ce8b1e1c6472974c0e239
kartik-125 <kartiksaini9034@gmail.com>
2025-12-27 00:38:27 +0530

    another commit

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import Navbar from '../components/Navbar'
 const Home = () => {
   return (
-    <div>
-      <h1>HOME</h1>
-    </div>
+    <>
+      <Navbar/>
+    </>
   )
 }
 
